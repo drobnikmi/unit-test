f54326e3712f36561e6fb3a09ccd1cfb6365c428
Michal Drobnik <michal.drobnik@onwelo.com>
2018-07-25 13:27:45 +0200

    remove duplicate function

--- src/app/thread.controller.js
+++ src/app/thread.controller.js
@@ -11,6 +11,29 @@ class ThreadController {
     removeElement(index){
         this.data.splice(index,1);
     }
+    removeDuplicatedWords(sentence){
+        if(typeof sentence !== 'string') throw new Error('Please provide string param');
+        if(sentence === '') throw new Error('String should have at least one char');
+        if(!sentence.includes(' ')) return sentence;
+        const sentenceArray = sentence.replace(/\s+/g, ' ').trim().split(' ');
+        // for(let sentence of sentenceArray) {
+        //     if(destinationArray.indexOf(sentence) === -1){
+        //         destinationArray.push(sentence);
+        //     }
+        // }
+        // return destinationArray.join(' '); 
+
+        // for(let sentence of sentenceArray) {
+        //     if(!destinationArray.includes(sentence)){
+        //         destinationArray.push(sentence);
+        //     }
+        // }
+        // return destinationArray.join(' ');
+
+        // return Array.from(new Set(sentenceArray)).join(' ');
+        return [...new Set(sentenceArray)].join(' ');
+        
+    }
 }
 
 export default ThreadController;
